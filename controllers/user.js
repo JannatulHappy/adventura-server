3372bef93c9af65527e2b934404114fdb6999bf6
JannatulHappy <jannatulferdoushappy21@gmail.com>
2022-04-27 01:14:05 +0600

    signin-done

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,35 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-
+// for business logic
 import UserModal from "../models/user.js";
 
 const secret = "test";
 
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
 
+  try {
+    const oldUser = await UserModal.findOne({ email });
+    if (!oldUser)
+      return res
+        .status(404)
+        .json({ message: "User doesn't exist.Please login" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password);
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid password" });
+
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ result: oldUser, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+    console.log(error);
+  }
+};
 
 export const signup = async (req, res) => {
   const { email, password, firstName, lastName } = req.body;
@@ -34,3 +58,25 @@ export const signup = async (req, res) => {
   }
 };
 
+export const googleSignIn = async (req, res) => {
+  const { email, name, token, googleId } = req.body;
+
+  try {
+    const oldUser = await UserModal.findOne({ email });
+    if (oldUser) {
+      const result = { _id: oldUser._id.toString(), email, name };
+      return res.status(200).json({ result, token });
+    }
+
+    const result = await UserModal.create({
+      email,
+      name,
+      googleId,
+    });
+
+    res.status(200).json({ result, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+    console.log(error);
+  }
+};
